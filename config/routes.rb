e6f07dcc4b80d8c57643cc5004d6d4e6dd86d481
dylan moutachi <dylan.moutachi@outlook.fr>
2024-05-27 17:56:21 +0200

    booking controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :products
-
-
+  resources :products do
+    resources :bookings, only: %i[index new create]
+  end
 end
