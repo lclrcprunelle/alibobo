523d3da3a85d68883a3135fcd8ee1fac4c257c68
Prunelle Leclerc <prunellelclrc@gmail.com>
2024-05-27 14:50:17 +0200

    Generate product model

---
+++ db/migrate/20240527124746_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.integer :price
+      t.string :category
+      t.string :description
+      t.float :rating
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
