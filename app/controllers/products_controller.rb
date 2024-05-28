b70c0cef58fcbd832628c42684e6e9249e7d8f71
Prunelle Leclerc <prunellelclrc@gmail.com>
2024-05-28 12:28:24 +0200

    version finale controller product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,6 +16,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     @product.user = current_user
+
     if @product.save
       redirect_to product_path(@product)
     else
@@ -24,17 +25,28 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path(@product)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
   end
 
   private
 
   def product_params
-    params.require(:product).permit(:title, :price, :category, :description, :rating, :photo)
+    params.require(:product).permit(:title, :price, :category, :description, :rating,:photo)
   end
 end
