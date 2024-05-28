3cce847dc95d1f24b1a10ba91f65833e21a0891a
victor dupont <victordupont1407@gmail.com>
2024-05-28 16:00:49 +0200

    booking

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :products do
-    resources :bookings, only: %i[index create]
+    resources :bookings, only: %i[index new create]
   end
 end
